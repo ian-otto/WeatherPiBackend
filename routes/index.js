e4e1f61695d383a7e952554f27fb69a7a7479cd7
Gennaro Napolitano <gennaro@nau.edu>
2019-04-27 17:12:03 -0700

    start work on mysql backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const MySQLConnector = require('../lib/mysql-connector');
+const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({app: 'WeatherPI V1.0'});
+    res.json({app: 'WeatherPI V1.0'});
+});
+
+router.get('/get_data', function(req, res, next) {
+    if(req.query.start_time === undefined) {
+        res.json({error: "start_time is a required parameter"});
+    } else {
+        let conn = MySQLConnector.getInstance().conn;
+        conn.query("SELECT * FROM", function(error, results, fields));
+        res.json({start_time: req.query.start_time, end_time: req.query.end_time});
+    }
 });
 
 module.exports = router;
